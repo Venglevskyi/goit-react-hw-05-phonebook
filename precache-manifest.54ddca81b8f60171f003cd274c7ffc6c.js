d8724af65272b1180f2ecac0cf278167cee94fe1
Roman <Roman.Vengl1@gmail.com>
2020-04-02 17:18:47 +0300

    Updates

---
+++ precache-manifest.54ddca81b8f60171f003cd274c7ffc6c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20464273309654e3512be25a02836353",
+    "url": "/goit-react-hw-05-phonebook/index.html"
+  },
+  {
+    "revision": "1046d9dc9b8d912d4c09",
+    "url": "/goit-react-hw-05-phonebook/static/css/2.f87e9f44.chunk.css"
+  },
+  {
+    "revision": "43a5146b870804483f4d",
+    "url": "/goit-react-hw-05-phonebook/static/css/main.367ccf75.chunk.css"
+  },
+  {
+    "revision": "1046d9dc9b8d912d4c09",
+    "url": "/goit-react-hw-05-phonebook/static/js/2.17a72297.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/goit-react-hw-05-phonebook/static/js/2.17a72297.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43a5146b870804483f4d",
+    "url": "/goit-react-hw-05-phonebook/static/js/main.d423d888.chunk.js"
+  },
+  {
+    "revision": "fd107b93bce7ea675eb7",
+    "url": "/goit-react-hw-05-phonebook/static/js/runtime-main.f6440cbd.js"
+  },
+  {
+    "revision": "d01e640f1dce53244f3c8738bc7fd807",
+    "url": "/goit-react-hw-05-phonebook/static/media/server-error.d01e640f.jpg"
+  }
+]);
